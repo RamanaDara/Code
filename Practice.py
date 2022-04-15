002c0552a74ef511cda22a3f4fb429f1b28a0b12
Ram <60974709+RamanaDara@users.noreply.github.com>
2022-04-15 21:22:01 +0530

    adding two list and remove common elements

--- Practice.py
+++ Practice.py
@@ -1,5 +1,9 @@
+# adding two list and remove common elements
+
 l1 = [1,2,3,4,5,6]
 l2 = [2,5,7,8,9,10]
-l1.extend(l2)
-print(l1)
 
+for i in l1:
+    if i not in l2:
+        l2.append(i)
+print(l2)    
